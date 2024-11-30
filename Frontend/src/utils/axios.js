9bd37db68b936e37ea97e0b4d35f779392fcdded
anuzza <anuja3580@gmail.com>
2024-11-30 16:57:28 -0600

    update url

--- Frontend/src/utils/axios.js
+++ Frontend/src/utils/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-// const url = "http:172.24.202.185:8000";
-const url = "http:192.168.0.93:8000";
+const url =
+  process.env.NODE_ENV === "production"
+    ? "https://rebel-threads.vercel.app"
+    : "http:192.168.0.93:8000";
 export default axios.create({
   baseURL: url,
 });
